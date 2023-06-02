e97e31a9bee8de3da158a2b60761f7fab1c061fa
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-06-02 19:23:35 -0400

    Initial commit

---
+++ src/main/java/com/allthing/lessonone/App.java
@@ -0,0 +1,14 @@
+package com.allthing;
+
+public class App {
+    //psvm -> public static void main(String[] args){}
+    /*
+    This
+    Is a multiline comment
+     */
+    public static void main(String[] args) {
+        //sout -> System.out.prinln();
+        System.out.println("Hello World!");
+        
+    }
+}
---
+++ src/main/java/com/allthing/lessonone/Strings.java
@@ -0,0 +1,2 @@
+package com.allthing.lessonone;public class Strings {
+}
